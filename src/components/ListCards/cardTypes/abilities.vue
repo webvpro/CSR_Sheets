ef84e4c6ca3b26d3b2b4fc0e14481d331e90fcc2
webvpro <mlstanley@gmail.com>
2022-09-12 19:41:00 -0700

    some fixes

---
+++ src/components/ListCards/cardTypes/abilities.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  itemData: { default: () => {}, type: Object },
+});
+const emit = defineEmits(["editItem", "copyItem"]);
+const cardType = computed(() => props.itemType.toLowerCase());
+</script>
+
+<template>
+  <div class="content">xx
+    <h3 class="inline-block">{{ itemData.abilityType }}</h3>
+    <h3 class="inline-block">
+      {{ itemData.statPool }} ({{ itemData.poolCost }})
+    </h3>
+  </div>
+</template>
