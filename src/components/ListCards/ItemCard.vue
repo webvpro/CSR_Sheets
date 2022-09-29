644175717b411222f27119c2c506fb36eb14e791
webvpro <mlstanley@gmail.com>
2022-09-29 10:06:42 -0700

    quick import fix

--- src/components/ListCards/ItemCard.vue
+++ src/components/ListCards/ItemCard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed } from "vue";
-import abilities from "@/components/ListCards/cardTypes/abilities.vue";
+import abilities from "@/components/ListCards/details/abilities.vue";
 import CollectionItemForm from "@/components/form/CollectionItemForm.vue";
 const props = defineProps({
   itemData: { default: () => {}, type: Object },
@@ -33,12 +33,42 @@ const cardType = computed(() => props.itemType.toLowerCase());
         v-html="itemData.description"
       />
       <component :is="cardType" :item-data="itemData"></component>
-      <div
-        class="justify-end flex-1 pt-2 border-t-4 btn-group card-actions border-primary"
-      >
+      <div class="btm-nav">
+        <button class="text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+        </button>
+        <button class="text-primary active">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
         <button
           v-if="allowEdit"
-          class="mt-auto btn btn-sm btn-primary text-primary-content btn-circle"
+          class="text-primary"
           @click="emit('openItem', itemData.itemData)"
         >
           <v-icon name="gi-gear-hammer" label="Edit" scale="1.5" />
---
+++ src/components/ListCards/details/ types.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  itemData: { default: () => {}, type: Object },
+});
+const emit = defineEmits(["editItem", "copyItem"]);
+const cardType = computed(() => props.itemType);
+</script>
+
+<template>
+  <div class="content">
+    <h3 class="inline-block capitalize">{{ itemData.abilityType }}</h3>
+    <h3 class="inline-block capitalize">
+      {{ itemData.costPool }} ({{ itemData.poolPoints }})
+    </h3>
+    <h3 class="inline-block capitalize">{{ itemData.abilityType }}</h3>
+    <h3 class="inline-block capitalize">
+      Tier: {{ itemData.costPool }} ({{ itemData.poolPoints }})
+    </h3>
+  </div>
+</template>
