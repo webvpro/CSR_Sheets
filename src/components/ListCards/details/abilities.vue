a79188872c277daeeeba0b56cb4a777d43d486f9
webvpro <mlstanley@gmail.com>
2022-09-29 09:38:19 -0700

    form work

--- src/components/ListCards/details/abilities.vue
+++ src/components/ListCards/details/abilities.vue
@@ -4,13 +4,13 @@ const props = defineProps({
   itemData: { default: () => {}, type: Object },
 });
 const emit = defineEmits(["editItem", "copyItem"]);
-const cardType = computed(() => props.itemType.toLowerCase());
+const cardType = computed(() => props.itemType);
 </script>
 
 <template>
   <div class="content">
-    <h3 class="inline-block">{{ itemData.abilityType }}</h3>
-    <h3 class="inline-block">
+    <h3 class="inline-block capitalize">{{ itemData.abilityType }}</h3>
+    <h3 class="inline-block capitalize">
       {{ itemData.statPool }} ({{ itemData.poolCost }})
     </h3>
   </div>
